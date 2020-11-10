b809da082df819fa21b41d9b9aec88d9ac88817f
Christerle Nounoute <christerlesn@gmail.com>
2020-11-10 04:29:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
 
 
   def self.create_from_collection(students_array)
-    students_array = Student.new(name:, location:)
+    students_array = Student.new(name, location)
     Scraper.scrape_index_page(students_array)
   end
 
