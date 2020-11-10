24027da1993a489bf553b4a1259782c8c4807cde
Christerle Nounoute <christerlesn@gmail.com>
2020-11-10 04:39:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    Scraper.scrape_profile_page(attributes_hash)
+    #Scraper.scrape_profile_page(attributes_hash)
   end
 
   def self.all
